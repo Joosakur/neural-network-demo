5cc1386e89b45a554d3337fdfcfda98a6fc43b26
Joosa Kurvinen <joosa.kurvinen@ardento.fi>
2021-02-15 21:18:42 +0200

    fix drawing on mobile

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -14,23 +14,22 @@ const Canvas = React.memo(function Canvas({
   height,
   scale
 }: Props) {
-  const handleMouseMove = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
-    if(e.buttons !== 1 || !canvasRef.current) return
 
+  const draw = (x: number, y: number, dx?: number, dy?: number) => {
+    if(!canvasRef.current) return
     const context = canvasRef.current.getContext('2d')
     if(!context) return
 
-    context.lineCap = 'round'
-    context.beginPath()
-    context.moveTo(e.nativeEvent.offsetX - e.nativeEvent.movementX, e.nativeEvent.offsetY - e.nativeEvent.movementY)
-    context.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY)
-    context.closePath()
-    context.lineWidth = 16
-    context.strokeStyle = `#000`
-    context.stroke()
-
-    const x = e.nativeEvent.offsetX
-    const y = e.nativeEvent.offsetY
+    if(dx && dy){
+      context.lineCap = 'round'
+      context.beginPath()
+      context.moveTo(x - dx, y - dy)
+      context.lineTo(x, y)
+      context.closePath()
+      context.lineWidth = 16
+      context.strokeStyle = `#000`
+      context.stroke()
+    }
 
     const gradient = context.createRadialGradient(x, y, 8, x, y, 20)
     gradient.addColorStop(0, "rgba(0, 0, 0, 1)")
@@ -43,18 +42,35 @@ const Canvas = React.memo(function Canvas({
     context.fill()
   }
 
+  const handleMouseMove = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+    if(e.buttons === 1 || !canvasRef.current) {
+      draw(e.nativeEvent.offsetX, e.nativeEvent.offsetY, e.nativeEvent.movementX, e.nativeEvent.movementY)
+    }
+  }
+
+  const handleTouchMove = (e: React.TouchEvent<HTMLCanvasElement>) => {
+    if(!canvasRef.current) return
+
+    if(e.changedTouches.length > 0) {
+      const touch = e.changedTouches.item(0)
+      draw(touch.pageX - canvasRef.current.offsetLeft, touch.pageY - canvasRef.current.offsetTop)
+    }
+  }
+
   return (
     <StyledCanvas
       ref={canvasRef}
       height={height * scale}
       width={width * scale}
       onMouseMove={handleMouseMove}
+      onTouchMove={handleTouchMove}
     />
   )
 })
 
 const StyledCanvas = styled.canvas`
   border: 1px solid black;
+  touch-action: none;
 `
 
 export default Canvas
